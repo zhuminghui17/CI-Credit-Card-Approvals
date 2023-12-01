6ee62b07c689fe5b625d3d38a68151f4fb389b62
Minghui Zhu <mz223@duke.edu>
2023-12-01 16:21:06 -0500

    Update files

--- lib.py
+++ lib.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 def read_data(path):
     """
     Read data from path
@@ -29,19 +28,86 @@ def describe_data(df):
 
 # info
 def info_data(df):
-
+    """
+    Info data
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe containing data
+    Returns
+    -------
+    info : pd.DataFrame
+        Dataframe containing info
+    """
     return df.info()
 
 
 def head_data(df):
+    """
+    Head data
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe containing data
+        Returns
+    ----------
+    head : pd.DataFrame
+        Dataframe containing head
+    """
     return df.head()
 
-
 def missing_data(df):
-
+    """
+    Missing data
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe containing data
+    Returns
+    -------
+    missing : pd.DataFrame
+        Dataframe containing missing
+    """
+    return df.isnull().sum()
 
 def null_data(df):
+    """
+    Null data
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe containing data
+    Returns
+    -------
+    null : pd.DataFrame
+        Dataframe containing null
+    """
+    return df.isna().sum()
 
 def histogram(df):
+    """
+    Histogram
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe containing data
+    Returns
+    -------
+    hist : pd.DataFrame
+        Dataframe containing histogram
+    """
+    return df.hist()
 
 def correlation_matrix(df):
+    """
+    Correlation matrix
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe containing data
+    Returns
+    -------
+    corr : pd.DataFrame
+        Dataframe containing correlation matrix
+    """
+    return df.corr()
